f58453eebe5e9234d8acbf8f70a3594a3a7bd467
HalfSuperNate <nkulis@gmail.com>
2023-09-15 15:23:00 -0500

    set collection links

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -25,10 +25,10 @@ const Home: NextPage = () => {
         <Link href="https://www.cryptocloudpunks.com/" rel="noopener noreferrer" target="_blank">CCP Website |</Link>
         
         <Link href="https://etherscan.io/address/0xc05B2990B0d1103CC08Df5bf7cde9B58ac36D8fF" rel="noopener noreferrer" target="_blank"> Citizen Contract :</Link>
-        <Link href="https://looksrare.org/collections/0xc05B2990B0d1103CC08Df5bf7cde9B58ac36D8fF" rel="noopener noreferrer" target="_blank"> Collection |</Link>
+        <Link href="https://opensea.io/assets/ethereum/0xc05b2990b0d1103cc08df5bf7cde9b58ac36d8ff/" rel="noopener noreferrer" target="_blank"> Collection |</Link>
 
         <Link href="https://etherscan.io/address/0x82895fE2945A8B19c5E511E5AA90Da3Aa27331eA" rel="noopener noreferrer" target="_blank"> Cyborg Contract :</Link>
-        <Link href="https://looksrare.org/collections/0x82895fE2945A8B19c5E511E5AA90Da3Aa27331eA" rel="noopener noreferrer" target="_blank"> Collection</Link>
+        <Link href="https://opensea.io/assets/ethereum/0x82895fE2945A8B19c5E511E5AA90Da3Aa27331eA/" rel="noopener noreferrer" target="_blank"> Collection</Link>
       </footer>
     </div>
   );
